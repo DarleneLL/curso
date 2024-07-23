16323d6b102d0bdf284f3917ed3dd6d3a717691d
DarleneLL <153782176+DarleneLL@users.noreply.github.com>
2024-07-23 20:50:41 -0300

    implementa atualização do curso

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -17,6 +17,58 @@ class CursoController {
         }
     }
 
+    async listaTodos(request, response) {
+        try {
+            const { nome } = request.query
+
+            const cursos = await Curso.findAll({
+                where: nome ? { nome: nome } : {},
+                attributes: [
+                    ['id', 'identificador'],
+                    'nome',
+                    'duracao'
+                ],
+                order: [['duracao', 'DESC']]
+            })
+
+            if(cursos.length === 0) {
+                response.status(404).json({ mensagem: 'Não foi encontrado nenhum curso' })
+            }
+
+            response.json(cursos)
+        } catch (error) {
+            response.status(500).json({
+                mensagem: 'Houve um erro ao listar os curso'
+            })
+        }
+    }
+
+    async atualizar(request, response) {
+        try {
+            const id = request.params.id
+            const dados = request.body
+
+            const curso = await Curso.findByPk(id)
+
+            if (!curso) {
+                response
+                    .status(404)
+                    .json({ mensagem: 'Não foi encontrado o curso' })
+            }
+
+            curso.nome = dados.nome
+            curso.duracao = dados.duracao
+            await curso.save()
+
+            response.json(curso)
+
+        } catch (error) {
+            response.status(500).json({
+                mensagem: 'Houve um erro ao atualiza o curso'
+            })
+        }
+    }
+
 }
 
 module.exports = new CursoController()
--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -4,5 +4,7 @@ const CursoController = require('../controllers/CursoController')
 const cursosRoutes = new Router()
 
 cursosRoutes.post('/', CursoController.criar)
+cursosRoutes.get('/', CursoController.listaTodos)
+cursosRoutes.put('/:id', CursoController.atualizar)
 
 module.exports = cursosRoutes
